aa0c521f62548c268223ee175c81358da5234876
hoanglong2534 <phamxuanhoanglong@gmail.com>
2026-02-05 23:24:52 +0700

    add config websocket

---
+++ server/src/main/java/com/learn/socket/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.learn.socket.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // client ket noi vao /ws
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // clien gui mesg vao /app/**
+        registry.setApplicationDestinationPrefixes("/app");
+
+        // server phan hoi bang /topic/**
+        registry.enableSimpleBroker("/topic");
+
+    }
+
+}
